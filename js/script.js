972d57127416333f6baa01d3e87b213c41ae4d57
Cretrok <45377681+Cretrok@users.noreply.github.com>
2019-06-18 00:37:50 -0500

    Funciones js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,34 @@ const respuestas = document.querySelector('.respuestas');
 let = 
 
 console.log(respuestas);
+
+class Question{
+  constructor(q, a){
+    this.question = q;
+    this.answer = a;
+  }
+  guess(res){
+    return res === this.answer;
+  }
+}
+
+class Quiz{
+  questions = [];
+  score = 0;|
+  start(){
+    this.questions.push(new Question('Quien soy yo?', 'Pues yo'));
+  }
+  next(){
+    const nextQuestion = this.questions.pop();
+    return nextQuestion;
+  }
+}
+
+const quiz = new Quiz();
+quiz.start();
+question = quiz.next();
+console.log(question);
+if(question.guess("Pues yo")){
+  quiz.score++;
+}
+console.log(quiz.score);
